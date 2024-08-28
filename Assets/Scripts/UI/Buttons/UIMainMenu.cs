c2188365deadd1d1fcc283d4d74e81dbd5373b9c
Joaquín Sacha Varisco <varisco54@gmail.com>
2024-08-28 12:08:58 -0300

    NewMenu

--- Assets/Scripts/UI/Buttons/UIMainMenu.cs
+++ Assets/Scripts/UI/Buttons/UIMainMenu.cs
@@ -11,7 +11,17 @@ public class UIMainMenu : MonoBehaviour
     [SerializeField] private GameObject playGO;
     private Button playButton;
     [SerializeField] private Button settingsButton;
+    [SerializeField] private GameObject settingsBackGO;
+    [SerializeField] private Button settingsBackButton;
     [SerializeField] private Button creditsButton;
+    [SerializeField] private GameObject creditsBackGO;
+    [SerializeField] private Button creditsBackButton;
+
+
+    [Header("Panels")]
+    [SerializeField] private GameObject settingsGO;
+    [SerializeField] private GameObject creditsGO;
+    
 
     [Header("Transitions")]
     [SerializeField] private TransitionSettings transition;
@@ -19,30 +29,55 @@ public class UIMainMenu : MonoBehaviour
 
     private void OnEnable()
     {
-
+        EventSystem.current.SetSelectedGameObject(playGO);
     }
     void Awake()
     {
+        //Menu buttons
         playButton = playGO.GetComponent<Button>();
+        settingsButton.onClick.AddListener(OnSelectedOptions);
         playButton.onClick.AddListener(OnSelectedPlay);
-    }
+        creditsButton.onClick.AddListener(OnSelectedCredits);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // BackButtons
+        settingsBackButton.onClick.AddListener(OnBackToMenu);
+        creditsBackButton.onClick.AddListener(OnBackToMenu);
     }
     private void OnDestroy()
     {
         playButton.onClick.RemoveAllListeners();
+        settingsButton.onClick.RemoveAllListeners();
+        creditsButton.onClick.RemoveAllListeners();
+        settingsBackButton.onClick.RemoveAllListeners();
+        creditsBackButton.onClick.RemoveAllListeners();
     }
 
     #region Canvas buttons functions
     private void OnSelectedPlay()
     {
-        
+
         TransitionManager.Instance().Transition("World", transition, loadDelay);
 
+    }
+    private void OnSelectedCredits()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(creditsBackGO);
+        creditsGO.SetActive(true);
+    }
+
+    private void OnSelectedOptions()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(settingsBackGO);
+        settingsGO.SetActive(true);
+    }
+    private void OnBackToMenu()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(playGO);
+        settingsGO.SetActive(false);
+        creditsGO.SetActive(false);
     }
     #endregion
 }
